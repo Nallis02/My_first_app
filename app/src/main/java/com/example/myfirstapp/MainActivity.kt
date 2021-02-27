77d4d8434cf3d868a174e7165f1911fc8516069c
Nallis <nallive14@gmail.com>
2021-02-26 20:46:25 -0500

    se crea boton y texto

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val photo = photo ("ejemplo", "12/10/2020", "des ejemplo")
+            button.setOnClickListener {
+            texto.text = photo.fecha
+        }
     }
 }
